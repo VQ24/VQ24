b5cef2666772640b62997543202889508079c416
Zlotsin Ivan <izlotsin@exadel.by>
2019-01-10 14:54:31 +0300

    Updates

---
+++ precache-manifest.0615881474782bee4ac1927fadafaf28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24729bc38deb2a6d01b9",
+    "url": "/VQ24/static/js/runtime~main.24729bc3.js"
+  },
+  {
+    "revision": "8ca7503bbe3716e98243",
+    "url": "/VQ24/static/js/main.8ca7503b.chunk.js"
+  },
+  {
+    "revision": "c86f41273b85ad61065e",
+    "url": "/VQ24/static/js/1.c86f4127.chunk.js"
+  },
+  {
+    "revision": "8ca7503bbe3716e98243",
+    "url": "/VQ24/static/css/main.053f24b3.chunk.css"
+  },
+  {
+    "revision": "037388d38e64c2af86901566b9545423",
+    "url": "/VQ24/index.html"
+  }
+];
